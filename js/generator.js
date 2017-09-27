d53538bd3115b3567bc3bcddc058ca0699590ec4
crosswalkgenerator <32223014+crosswalkgenerator@users.noreply.github.com>
2017-09-27 13:33:16 -0400

    Update generator.js

--- js/generator.js
+++ js/generator.js
@@ -1,4 +1,19 @@
-
-$("#first-choice").change(function() {
-   $("#second-choice").load("textdata/" + $(this).val() + ".txt");
+jQuery(function($) {
+    var standard2 = {
+        'Dublin Core': ['MARC', 'METS', 'MODS', 'VRA Core'],
+        'MARC': ['Dublin Core', 'METS', 'MODS', 'VRA Core'],
+        'METS': ['Dublin Core', 'MARC', 'MODS', 'VRA Core'],
+        'MODS': ['Dublin Core', 'MARC', 'METS', 'VRA Core'],
+        'VRA Core': ['Dublin Core', 'MARC', 'METS', 'MODS'],
+    }
+    
+    var $standard2 = $('#location');
+    $('#standard1').change(function () {
+        var standard1 = $(this).val(), std2 = standard2[standard1] || [];
+        
+        var html = $.map(std2, function(std){
+            return '<option value="' + std + '">' + std + '</option>'
+        }).join('');
+        $standard2.html(html)
+    });
 });
